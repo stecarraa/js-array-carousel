b5c0a8fc7f8673949b08ba0d80a9f832d0a44332
stecarraa <stefano@caratozzolo.me>
2022-06-20 15:53:08 +0200

    start structure ok

---
+++ main.js
@@ -0,0 +1,35 @@
+const givenImages = [
+    'https://cdn.photographycourse.net/wp-content/uploads/2022/04/Portrait-vs-Landscape-Featured-Image-3.jpg',
+    'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg',
+    'https://cdn.photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg',
+    'https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg',
+    'https://iso.500px.com/wp-content/uploads/2021/02/Torres-Del-Paine-Sunset-By-Paul-Reiffer-2-1500x1000.jpg',
+    'https://mymodernmet.com/wp/wp-content/uploads/2020/02/Landscape-Photographer-of-the-Year-Sander-Grefte.jpg'
+    ];
+
+
+
+
+
+
+// immagine da cui voglio partire nell'array
+let activeElementIndex = 0;
+
+
+// mi vado a prendere dove visualizzare le img nel dome
+const imgsWrapper = document.querySelector('.carousel-container');
+const imgList = imgsWrapper.children;
+
+// creo l'elemento html di tipo img
+const newImg = document.createElement('img');
+
+// aggiungo le classi che voglio che questo abbia
+newImg.classList.add('w-100', 'd-bolck');
+
+// ? cambio l'src dell'immagine => .setAttribute(quale attributo, contenuto dell'attributo)
+newImg.setAttribute('src', 'https://cdn.photographycourse.net/wp-content/uploads/2022/04/Portrait-vs-Landscape-Featured-Image-3.jpg');
+
+// | aggiungo l'immagine creata alla lista
+imgsWrapper.append(newImg);
+
+imgList[activeElementIndex].classList.add('active');
